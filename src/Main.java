b4738a20b8e87c7ab40e0f4ab222fd5af5c7612d
IamYourFatherMySon <ttt.rrrmtop@gmail.com>
2021-03-18 15:31:21 +0100

    I made you my son...

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,16 @@ public class Main {
         UsedCars car2 = new UsedCars(4, "Mercedes Benz", 24000, 54000);
         System.out.println(car2.getFinalPrice());
 
+        UsedCars car3 = new UsedCars(4, "Bentley", 4000000, 660000);
+        System.out.println(car3.getFinalPrice());
+
         damagedCars car4 = new damagedCars(4, "Mercedes Benz", 78000, 2);
         System.out.println(car4.getFinalPrice());
 
+
         Fahrzeug motorrad = new Motorrad(2, "Yamaha", 7000);
         motorrad.drive(); //dynamic binding
-        car.drive();
+        car.drive(); //3
     }
 
 }
